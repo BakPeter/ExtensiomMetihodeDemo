fe733781a947f58ec30b6c74631a9b0466e98fb7
BakPeter <bak.pet@gmail.com>
2022-06-23 11:07:42 +0300

    Add project files.

---
+++ ExtensiomMetihodeDemo/IntExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtensiomMetihodeDemo;
+
+public static class IntExtension
+{
+    public static int Pow(this int num, int exp)
+    {
+        return (int)Math.Pow(num, exp);
+    }
+
+    public static string ConcatTwoNumsToToStr(this int num1, int num2, string str)
+    {
+        return $"{num2} - {str} - {num1}";
+    }
+}
---
+++ ExtensiomMetihodeDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensiomMetihodeDemo;
+
+Console.WriteLine("Hello, World!");
+
+int n1 = 2;
+Console.WriteLine(n1.Pow(3));
+Console.WriteLine(n1.ConcatTwoNumsToToStr(4, "peter"));
